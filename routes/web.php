035412141898ef4c74e7daa7c31eed9ff3b2f97c
crisander23 <84034798+crisander23@users.noreply.github.com>
2023-04-13 11:51:35 +0800

    first commit for api

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+Route::get('/user/{id}', function ($id) {
+    $user = DB::table('users')->where('id', $id)->first();
+    return response()->json([
+        'name' => $user->name,
+        'email' => $user->email
+    ]);
+});
